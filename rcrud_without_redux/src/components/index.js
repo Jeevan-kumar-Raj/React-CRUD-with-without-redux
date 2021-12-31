3f38d7e96b7108986fbb97be2aa086aa9fe7cbe3
Jeevan Kumar <63276441+Jeevan-kumar-Raj@users.noreply.github.com>
2021-12-31 19:55:04 +0530

    Add files via upload

---
+++ rcrud_without_redux/src/components/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"; 
+
+class  extends Component {
+  render() {
+    return (
+     <div>
+       
+     </div>
+    );
+  }
+}
+
+export default ;
+
+import React, { Component } from "react"; 
+
+class  extends Component {
+  render() {
+    return (
+     <div>
+       
+     </div>
+    );
+  }
+}
+
+export default ;
